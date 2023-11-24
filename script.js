458472c0303a42f438f33c7f653c54978d34bea0
JC2295 <64856330+JC2295@users.noreply.github.com>
2023-11-24 14:39:50 +0000

    Added core functionality.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let numberContainer = document.querySelector("#number");
+
+numberContainer.style = "font-size: 2rem";
+
+let number = 0;
+
+numberContainer.innerText = number;
+
+let countButton = document.querySelector("#count-btn");
+
+let resetButton = document.querySelector("#reset-btn");
+
+countButton.addEventListener("click", addNumber);
+
+resetButton.addEventListener("click", resetNumber);
+
+function addNumber() {
+
+  number++;
+
+  numberContainer.innerText = number;
+
+}
+
+function resetNumber() {
+
+  number = 0;
+
+  numberContainer.innerText = number;
+
+}
 
